ba5c67ee748f66e536db68834e2dec69282954c5
eriqro <142878819+eriqro@users.noreply.github.com>
2024-08-21 11:12:52 +0200

    dasda

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 # Dice sum probability calculator
-# Författare: 
+# Författare: eric
 # Datum:
+from collections import Counter
+
+def most_frequent(List):
+    occurence_count = Counter(List)
+    print( occurence_count.most_common(1)[0][0])
 
 def main():
-    user_input = input().split(" ")
+    user_input = input("Skriv två nummer för tärningarna: ").split(" ")
+    diceone=int(user_input[0])
+    dicetwo=int(user_input[1])
+    dicesum=[]
+    for i in range(1,diceone+1):
+        for x in range(1,dicetwo+1):
+            throw=x+i
+            dicesum.append(throw)
+    most_frequent(dicesum)
 
+    
 if __name__ == "__main__":
     main()
